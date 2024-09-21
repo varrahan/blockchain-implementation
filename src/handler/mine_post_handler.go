7251a9339fd47571c04ab33d9bfb9d68d7759fbe
varrahan <varrahan@gmail.com>
2024-09-21 01:29:15 -0400

    "Update mine post handler with comments"

--- src/handler/mine_post_handler.go
+++ src/handler/mine_post_handler.go
@@ -5,13 +5,15 @@ import (
 	"net/http"
 )
 
-func (ctx *HandlerContext) Mine_Post_handler(w http.ResponseWriter, r *http.Request) {
-    minerAddress := r.URL.Query().Get("address")
-    if minerAddress == "" {
-        http.Error(w, "Miner address is required", http.StatusBadRequest)
-        return
-    }
+func (ctx *HandlerContext) MineBlock_Post_handler(w http.ResponseWriter, r *http.Request) {
+	// Get address from query parameters
+	minerAddress := r.URL.Query().Get("address")
+	// If address is blank raise error
+	if minerAddress == "" {
+		http.Error(w, "Miner address is required", http.StatusBadRequest)
+		return
+	}
 	ctx.Blockchain.MineBlock(minerAddress)
-    w.WriteHeader(http.StatusCreated)
-    json.NewEncoder(w).Encode(ctx.Blockchain.Blocks[len(ctx.Blockchain.Blocks)-1])
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(ctx.Blockchain.Blocks[len(ctx.Blockchain.Blocks)-1])
 }
