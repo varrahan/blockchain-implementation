1c17c8ac6f40f61415cb4fb38a9336c95e1a44d8
varrahan <varrahan@gmail.com>
2024-09-21 14:15:06 -0400

    Use gorrila mux's .Vars() method for better performance

--- src/handler/balance_get_handler.go
+++ src/handler/balance_get_handler.go
@@ -3,10 +3,15 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+
+	mux "github.com/gorilla/mux"
 )
 
 func (ctx *HandlerContext) GetBalance_Get_Handler(w http.ResponseWriter, r *http.Request) {
-	address := r.URL.Query().Get("address")
+	// Get address of user with query parameter
+	vars := mux.Vars(r)
+	address := vars["address"]
+	// Get the balance as a float of each transaction where address existed
 	balance := ctx.Blockchain.GetBalance(address)
 	json.NewEncoder(w).Encode(map[string]float64{"balance": balance})
 }
