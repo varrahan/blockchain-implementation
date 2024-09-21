655b53f92a4f3e1862d17e17c99f7088107b20cc
varrahan <varrahan@gmail.com>
2024-09-21 14:15:29 -0400

    Use gorrila mux's .Vars() method for better performance

--- src/handler/mine_post_handler.go
+++ src/handler/mine_post_handler.go
@@ -3,16 +3,20 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+
+	mux "github.com/gorilla/mux"
 )
 
 func (ctx *HandlerContext) MineBlock_Post_handler(w http.ResponseWriter, r *http.Request) {
 	// Get address from query parameters
-	minerAddress := r.URL.Query().Get("address")
+	vars := mux.Vars(r)
+	minerAddress := vars["address"]
 	// If address is blank raise error
 	if minerAddress == "" {
 		http.Error(w, "Miner address is required", http.StatusBadRequest)
 		return
 	}
+	// Mine the block to add new block to blockchain
 	ctx.Blockchain.MineBlock(minerAddress)
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(ctx.Blockchain.Blocks[len(ctx.Blockchain.Blocks)-1])
