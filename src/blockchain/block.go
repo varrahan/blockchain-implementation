1526e6df56cd63f0fd3d8b5f0e2226d2360e8041
varrahan <varrahan@gmail.com>
2024-09-21 14:14:18 -0400

    Lint block.go

--- src/blockchain/block.go
+++ src/blockchain/block.go
@@ -5,24 +5,24 @@ import (
 )
 
 type Block struct {
-	Index 		  	int64
-	Timestamp     	time.Time
-	Transactions 	[]*Transaction
-	PrevBlockHash 	[]byte
-	Hash         	[]byte
-	Nonce         	int
-	Difficulty    	int
+	Index         int64
+	Timestamp     time.Time
+	Transactions  []*Transaction
+	PrevBlockHash []byte
+	Hash          []byte
+	Nonce         int
+	Difficulty    int
 }
 
 // Creates a new block to add to blockchain
 func NewBlock(index int64, transactions []*Transaction, prevHash []byte, difficulty int) *Block {
 	// Create a new block
 	newBlock := &Block{
-		Index: 			index,
-		Timestamp: 		time.Now(),
-		Transactions: 	transactions,
-		PrevBlockHash:	prevHash,
-		Difficulty: 	difficulty}
+		Index:         index,
+		Timestamp:     time.Now(),
+		Transactions:  transactions,
+		PrevBlockHash: prevHash,
+		Difficulty:    difficulty}
 	// Create proof of work for new block
 	pow := NewProofOfWork(newBlock)
 	// Run proof of work to find and set the nonce and hash
